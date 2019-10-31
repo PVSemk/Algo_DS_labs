740b1b04237799306efcc8fa109ccd35b719b09e
Pavel Semkin <pvsemk@mail.ru>
2019-10-31 12:23:07 +0300

    Implemented custom deepcopy

--- lab_2/logic/moves.py
+++ lab_2/logic/moves.py
@@ -1,10 +1,12 @@
 from .board import BoardState, EmptyLocation
+from copy import deepcopy
 
 
 def make_moves(state):
     next_states = []
     empty_row = state.empty_cell.x
     empty_col = state.empty_cell.y
+    old_coordinates = (empty_row, empty_col)
 
     left = (empty_row, empty_col - 1)
     right = (empty_row, empty_col + 1)
@@ -12,13 +14,10 @@ def make_moves(state):
     down = (empty_row + 1, empty_col)
 
     for new_coordinates, direction in zip([left, up, right, down], ['Left', 'Up', 'Right', 'Down']):
-        (new_empty_row, new_empty_col) = new_coordinates
-
-        if 0 <= new_empty_row < 3 and 0 <= new_empty_col < 3:
-            new_state = [r[:] for r in state.state]  # Taking slice in order to copy values, not reference to list
-            new_state[empty_row][empty_col], new_state[new_empty_row][new_empty_col] =  \
-                new_state[new_empty_row][new_empty_col], state.state[empty_row][empty_col]
-
-            next_states.append(BoardState(new_state, EmptyLocation(new_empty_row, new_empty_col), state, direction))
+        if 0 <= new_coordinates[0] < 3 and 0 <= new_coordinates[1] < 3:
+            new_state = deepcopy(state)
+            new_state.parent = state
+            new_state.swap_with_empty(old_coordinates, new_coordinates, direction)
+            next_states.append(new_state)
 
     return next_states
